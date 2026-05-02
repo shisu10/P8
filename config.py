5f6d4da6ab05eb0c1d06992d80fc16d10c5aefe7
1 2 <2318709388@qq.com>
2026-05-03 00:53:25 +0800

    complete database functions 1.2

--- config.py
+++ config.py
@@ -37,6 +37,12 @@ class Config(BaseSettings):
     JWT_SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     JWT_ALGORITHM = "HS256"
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 24 * 60
+    # 数据库
+    DATABASE_CHOICES = {"normal1" : "mysql+pymysql://root:123456@localhost:3306/test_db",
+                        "fake1" : "mysql+pymysql://root:123456@localhost:3306/test_db"
 
+    }
+    DATABASE_URL: str = "mysql+pymysql://root:123456@localhost:3306/test_db"
+    DATABASE_ECHO: bool = True  # 是否打印 SQL
 
 settings = Config()
